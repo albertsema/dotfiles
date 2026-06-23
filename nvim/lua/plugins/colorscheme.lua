4178f48da72fc1c366acf3facca098cf1319cd68
Albert Sema <amatabar@cisco.com>
2026-06-23 12:51:34 -0400

    change colorscheme and themes to rose pine

---
+++ nvim/lua/plugins/colorscheme.lua
@@ -0,0 +1,16 @@
+return {
+  {
+    "rose-pine/neovim",
+    name = "rose-pine",
+    opts = {
+      variant = "main",
+      dark_variant = "main",
+    },
+  },
+  {
+    "LazyVim/LazyVim",
+    opts = {
+      colorscheme = "rose-pine",
+    },
+  },
+}
